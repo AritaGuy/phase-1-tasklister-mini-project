5091d8c40ec6f72bcd075993ad0732ad3275668c
Sam Arita <arita.sam@student.moringaschool.com>
2022-08-23 15:42:35 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) =>{
+    e.preventDefault()
+    handleToDo(e.target.new_task.value)
+    //taskList.reset()
+  })
 });
+
+function handleToDo(todo){
+  let li = document.createElement('li')
+  li.textContent = todo
+  let delButt = document.createElement('button')
+  delButt.textContent = 'x '
+  li.appendChild(delButt)
+  document.querySelector('#tasks').appendChild(li) 
+  delButt.addEventListener('click', handleDelete)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
